c1001719dc20cceca65a123503d9fd5eaebe576c
Thanh Luan Vo <vtluan.nt.1999@gmail.com>
2020-04-26 10:21:50 +0700

    BaiTap5

---
+++ BaiTapTuan2/BaiTap5/BaiTap5/Controllers/BannerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap5.Controllers
+{
+    public class BannerController : Controller
+    {
+        // GET: Banner
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangeBanner(HttpPostedFileBase banner)
+        {
+            String postedFileName = System.IO.Path.GetFileName(banner.FileName);
+            var path = Server.MapPath("/Images/" + postedFileName);
+            banner.SaveAs(path);
+            String fSave = Server.MapPath("/banner.txt");
+            System.IO.File.WriteAllText(fSave, postedFileName);
+
+            return View("Index");
+        }
+    }
+}
