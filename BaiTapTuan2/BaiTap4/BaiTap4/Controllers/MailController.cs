7ad79b5df9b944cd9aedfc43b2c307773966b1fa
Thanh Luan Vo <vtluan.nt.1999@gmail.com>
2020-04-26 09:54:39 +0700

    BaiTap4

---
+++ BaiTapTuan2/BaiTap4/BaiTap4/Controllers/MailController.cs
@@ -0,0 +1,38 @@
+﻿using BaiTap4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap4.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(MailModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            mail.From = new System.Net.Mail.MailAddress(model.From);
+            mail.To.Add(model.To);
+            mail.Subject = model.Subject;
+            mail.Body = model.Body;
+            mail.IsBodyHtml = true;
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new System.Net.NetworkCredential(model.From, model.Password);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BaiTapTuan2/BaiTap4/BaiTap4/Models/MailModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap4.Models
+{
+    public class MailModel
+    {
+        [DisplayName("Gửi từ Email")]
+        public String From { get; set; }
+
+        [DisplayName("Mật khẩu")]
+        public String Password { get; set; }
+
+        [DisplayName("Gửi đến Email")]
+        public String To { get; set; }
+
+        [DisplayName("Tiêu đề")]
+        public String Subject { get; set; }
+
+        [DisplayName("Nội dung")]
+        public String Body { get; set; }
+    }
+}
